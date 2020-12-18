6b8de2ba1b4e60920037a365d5e2bed72294a28d
kailash yadav <49616380+sidyadav2211@users.noreply.github.com>
2020-12-18 20:31:54 +0530

    Recursion Using Loops
    Factorial.

---
+++ UsingLoop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int num,f=1;
+    printf("Enter the Number :");
+    scanf("%d",&num);
+
+    for(int i=1; i<=num;i++){
+        f= f * 1 ;
+       
+    }
+    printf("Factorial of Number is %d:%d",num,f);
+    return 0;
+}
